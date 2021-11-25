a296dbae09085ca77a2a677648c74596068a2ea5
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2021-11-25 15:57:28 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+flavours = ["C", "S", "V", "M", "P", "R"]
+
+try:
+  print(f"ben inc. Flavour Combo gen\nFlavours: {flavours}\n")
+
+  printed = []
+  nofindattempts, attempts = 0, 0
+  while True:
+    attempts += 1
+    nofindattempts += 1
+
+    random.shuffle(flavours)
+    choice = random.randint(1, len(flavours))
+
+    icecream = sorted(flavours[0:choice])
+    endicecream = str()
+    for index in range(0, choice):
+      endicecream += icecream[index]
+  
+    if not endicecream in printed:
+      printed.append(endicecream)
+      nofindattempts = 0
+      print(f"{attempts:04}", endicecream)
+    elif nofindattempts > 5000:
+      print(f"\nResult: there's {len(printed)} possible combinations in {len(flavours)} flavours. finished in {attempts} attempts.")
+      break
+except(KeyboardInterrupt):
+  print(" ", len(printed))
